af3137ecd267652aeabfef1db422ffcb63f6f741
Rebeca Noemi <rebecanoemi494@gmail.com>
2024-12-30 17:25:45 -0300

    Update fase1(pacman).asm

--- fase1(pacman).asm
+++ fase1(pacman).asm
@@ -1,34 +1,39 @@
-.data
+# Fase 1 Jogo Rio
+# Azul do fundo: 0x546c8f 
+# Preto das paredes: 0x000000
+# salva o fundo em outro lugar da memoria: 131084($8) 
+# Cinza grades: 0xb4b4b4
+#
+#
+#
+#
+
 .text
 # O passáro branco tem que ser do tamanho do espaço entre as paredes verdes, pra ele poder se locomover atrás do blue
 # pq o blue é menor que o passáro branco no filme
 
 main:lui $8, 0x1001	   # fundo
-     lui $7, 0x1001	   # parte verde
-     ori $9, $0, 0x546c8f  # azul claro
-     ori $10, $0, 0x22b14c # verde claro
+     lui $7, 0x1001	   # muros
+     ori $9, $0, 0x2f3699  # Azul escuro
+     ori $10, $0, 0x000000 # Preto
+     ori $11, $0, 0xb4b4b4 # Preto
     
-     addi $20, $0, 7296    # Tamanho da tela toda
-     # TECLAS DE COMANDO DO JOGADOR
-     addi $11, $0, 'a'
-     addi $12, $0, 'd'
-     addi $13, $0, 's'
-     addi $14, $0, 'w'	
+     addi $20, $0, 8192    # Tamanho da tela toda
      addi $15 $0 16
 FundoAzul: # Pinta todo o fundo azul
-      beq $20, $0, MurosVerdes_emL
+      beq $20, $0, MurosPretos_emL
       sw $9, 0($8)
-      sw $9, 2048($8) # Salva para backup 
+      sw $9, 131084($8) # Salva para backup 
       addi $8, $8, 4
       addi $20, $20, -1
       j FundoAzul
-MurosVerdes_emL:
+##########################################################################
+# Paredes Pretas
+MurosPretos_emL:
       	parte_de_cima: 
       		beq $15 $0 fim_parte_cima
-      		sw $10 640($7) # parte de cima do L1
-      		sw $9, 2048($8) # Salva para backup 
-      		sw $10 832($7) # parte de cima do L2
-      		sw $9, 2048($8) # Salva para backup 
+      		sw $10 640($7)  # parte de cima do L1
+      		sw $10 832($7)  # parte de cima do L2
       		addi $7 $7 4
       		addi $15 $15 -1
       		j parte_de_cima
@@ -37,9 +42,11 @@ MurosVerdes_emL:
 	parte_laterais:
 		beq $15 $0 fim_laterais
 		sw $10 572($7)  # parte de cima do L1
-		sw $9, 2048($8) # Salva para backup 
       		sw $10 832($7)  # parte de cima do L2
-      		sw $9, 2048($8) # Salva para backup 
+      		
+      		sw $10 27708($7)  # parte de cima do L1
+      		sw $10 27968($7)  # parte de cima do L2
+      		
       		addi $7 $7 512
       		addi $15 $15 -1
 		j parte_laterais
@@ -48,16 +55,15 @@ MurosVerdes_emL:
 jaula_central:
 	addi $15 $0 24
 	laterais_da_jaula: 
-			beq $15 $0 fim_laterais_da_jaula
-			sw $10 5180($7) # parte lateral da jaula
-			sw $9, 2048($8) # Salva para backup 
-			
-			sw $10 5440($7) # parte lateral da jaula
-			sw $9, 2048($8) # Salva para backup 
+			beq $15 $0 fim_laterais_jaula
+			sw $10 5180($7) # parte lateral esquerda da jaula 
+			sw $10 5140($7) # parte lateral esquerda externa da jaula
+			sw $10 5440($7) # parte lateral direita da jaula
+			sw $10 5480($7) # parte lateral direita externa da jaula
 			addi $7 $7 512
 			addi $15 $15 -1
 			j laterais_da_jaula
-	fim_laterais_da_jaula:
+	fim_laterais_jaula:
 	addi $15 $0 64
 		for:    # for para voltar ao endereço incial das laterais da jaula
 			beq $15 $0 fimfor
@@ -69,9 +75,7 @@ jaula_central:
 	parte_superior_da_jaula:
 			beq $15 $0 fim_superior_da_jaula
 			sw $10 5180($7) # parte lateral da jaula
-			sw $9, 2048($8) # Salva para backup
 			sw $10 5360($7)
-			sw $9, 2048($8) # Salva para backup
 			addi $7 $7 4
 			addi $15 $15 -1
 			j parte_superior_da_jaula
@@ -85,12 +89,64 @@ jaula_central:
 		fimfor2:
 		addi $15 $0 66
 		parte_inferior:
-			beq $15 $0 fim_parte_superior
-			sw $10 11384($7) # parte lateral da jaula
-			# sw $9, 2048($8) # Salva para backup
+			beq $15 $0 fim_parte_superior_jaula
+			sw $10 17468($7) # parte lateral da jaula
 			addi $15 $15 -1
 			addi $7 $7 4
 			j parte_inferior
-		fim_parte_superior:
-		# faz mais sentido começar da parte superior depois ir pra lateral e terminar a parte inferior, do que ficar rodando que nem barata tonta
+		fim_parte_superior_jaula: # parte superior borda preta
+		lui $7, 0x1001
+		addi $15 $0 256
+		parte_superior_inferior:
+				beq $15 $0 fimParteSuperior
+				sw $10 0($7)     # parte superior
+				sw $10 31740($7) # parte inferior
+				addi $7 $7 4
+				addi $15 $15 -1
+				j parte_superior_inferior
+		fimParteSuperior:
+		lui $7, 0x1001
+		addi $15 $0 64
+		partes_laterais:
+			beq $15 $0 fimlaterais
+				sw $10 0($7)     # parte esquerda
+				sw $10 508($7)   # parte direita
+				addi $7 $7 512
+				addi $15 $15 -1
+				j partes_laterais
+		fimlaterais:
+		addi $15 $0 10
+		lui $8, 0x1001
+		entrada_saida: 	beq $15 $0 fim_entrada_saida
+				sw $9 13312($8)
+				sw $9, 131084($8) # salva para bachup
+				sw $9 13820($8)
+				sw $9, 131084($8) # salva para bachup
+				addi $8 $8 512
+				addi $15 $15 -1
+				j entrada_saida
+		fim_entrada_saida:
+	 	lui $7, 0x1001
+		addi $15 $0 8	
+murinho_frente_da_entrada_saida:
+		beq $15 $0 fim_murinho
+		sw $10 14900($7) # esquerda
+		sw $10 15276($7) # esquerda
+		addi $7 $7 4
+		addi $15 $15 -1
+		j murinho_frente_da_entrada_saida
+	fim_murinho:
+		lui $7, 0x1001
+		addi $15 $0 8
+	quadrados_superior:
+		parte_superior_quadrado:
+			beq $15 $0 parte_laterais_quadrado
+			sw $10 3636($7)  
+			sw $10 4012($7) 
+			sw $10 28212($7)  
+			sw $10 28588($7) 
+			addi $15 $15 -1
+			addi $7 $7 4
+			j parte_superior_quadrado
+		parte_laterais_quadrado:
 			
